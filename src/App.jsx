332e781a761ca7fdf1ea7cfa53c52040e3aabe86
burakozbudak <burakozbudak25@hacettepe.edu.tr>
2025-07-22 05:29:22 +0300

    app implementations
    actions ve reducer işlemlerini app dosyasında verilen sayılara ve istenen işlemlere uygun bir şekilde uygulandı

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import TotalDisplay from "./components/TotalDisplay.jsx";
 import CalcButton from "./components/CalcButton.jsx";
+import { useReducer } from "react";
+import { reducer, initialState } from "./store/reducers.jsx"; 
+import { 
+  applyNumber, 
+  changeOperation, 
+  clearDisplay, 
+  result,
+  memoryAdd,
+  memoryRecall, 
+  memoryClear
+} from "./store/actions.jsx";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
       <nav className="text-center py-6 px-4 bg-stone-800 text-white text-xl/none">
@@ -10,50 +23,50 @@ function App() {
 
       <div className="mt-12 mx-auto w-75">
         <form name="Cal">
-          <TotalDisplay value={"X"} />
+          <TotalDisplay value={state.total} />
           <div className="flex justify-between bg-black text-white text-sm px-3 py-2">
             <span id="operation">
-              <b>Operation:</b> X
+              <b>Operation:</b> {state.operation}
             </span>
             <span id="memory">
-              <b>Memory:</b> X
+              <b>Memory:</b> {state.memory}
             </span>
           </div>
           <div className="flex">
-            <CalcButton value={"M+"} />
-            <CalcButton value={"MR"} />
-            <CalcButton value={"MC"} />
+            <CalcButton value={"M+"} onClick = {() => dispatch(memoryAdd())} />
+            <CalcButton value={"MR"} onClick = {() => dispatch(memoryRecall())} />
+            <CalcButton value={"MC"} onClick = {() => dispatch(memoryClear())} />
           </div>
           <div className="flex">
-            <CalcButton value={1} />
-            <CalcButton value={2} />
-            <CalcButton value={3} />
-          </div>
-
-          <div className="flex">
-            <CalcButton value={4} />
-            <CalcButton value={5} />
-            <CalcButton value={6} />
+            <CalcButton value={1} onClick = {() => dispatch(applyNumber(1))} />
+            <CalcButton value={2} onClick = {() => dispatch(applyNumber(2))} />
+            <CalcButton value={3} onClick = {() => dispatch(applyNumber(3))} />
           </div>
 
           <div className="flex">
-            <CalcButton value={7} />
-            <CalcButton value={8} />
-            <CalcButton value={9} />
-          </div>
-          <div className="flex">
-            <CalcButton value={"+"} />
-            <CalcButton value={0} />
-            <CalcButton value={"-"} />
-          </div>
-          <div className="flex">
-            <CalcButton value={"*"} />
-            <CalcButton value={"/"} />
-            <CalcButton value={"CE"} />
+            <CalcButton value={4} onClick = {() => dispatch(applyNumber(4))} />
+            <CalcButton value={5} onClick = {() => dispatch(applyNumber(5))} />
+            <CalcButton value={6} onClick = {() => dispatch(applyNumber(6))} />
           </div>
 
           <div className="flex">
-            <CalcButton value={"="} />
+            <CalcButton value={7} onClick = {() => dispatch(applyNumber(7))} />
+            <CalcButton value={8} onClick = {() => dispatch(applyNumber(8))} />
+            <CalcButton value={9} onClick = {() => dispatch(applyNumber(9))} />
+          </div>
+          <div className="flex">
+            <CalcButton value={"+"} onClick = {() => dispatch(changeOperation("+"))} />
+            <CalcButton value={0} onClick = {() => dispatch(applyNumber(0))} />
+            <CalcButton value={"-"} onClick = {() => dispatch(changeOperation("-"))} />
+          </div>
+          <div className="flex">
+            <CalcButton value={"*"} onClick = {() => dispatch(changeOperation("*"))} />
+            <CalcButton value={"/"} onClick = {() => dispatch(changeOperation("/"))} />
+            <CalcButton value={"CE"} onClick = {() => dispatch(clearDisplay())} />
+          </div>
+
+          <div className="flex">
+            <CalcButton value={"="} onClick = {() => dispatch(result())} />
           </div>
         </form>
       </div>
